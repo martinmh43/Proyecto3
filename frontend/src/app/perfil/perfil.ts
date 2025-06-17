1df9295c1235cc272b8157df233ee69f3481c73d
Martín <145580847+martinmh43@users.noreply.github.com>
2025-06-17 11:03:13 +0200

    Ole ole ya ta

--- frontend/src/app/perfil/perfil.ts
+++ frontend/src/app/perfil/perfil.ts
@@ -14,40 +14,44 @@ import { Router } from '@angular/router';
 export class Perfil implements OnInit {
   usuario: any = {};
   intercambios: any[] = [];
+  pegatinas: any[] = [];
   cursoId!: number;
-
 
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
-  const urlParams = this.router.url.split('/');
-  this.cursoId = Number(urlParams[2]);  
+    const urlParams = this.router.url.split('/');
+    this.cursoId = Number(urlParams[2]);
 
-  this.http.get('http://localhost:8000/api/auth/profile/').subscribe(data => {
-    this.usuario = data;
-    if (this.usuario.role === 'profesor') {
-      this.cargarHistorial(); 
-    }
-  });
-}
-
-  
- cargarHistorial() {
-  if (!this.cursoId) {
-    console.error('cursoId inválido');
-    return;
+    this.http.get('http://localhost:8000/api/auth/profile/').subscribe(data => {
+      this.usuario = data;
+      if (this.usuario.role === 'profesor') {
+        this.cargarHistorial();
+      } else if (this.usuario.role === 'alumno') {
+        this.cargarPegatinas();
+      }
+    });
   }
 
-  this.http.get<any[]>(`http://localhost:8000/api/cursos/historial-intercambios/?curso=${this.cursoId}`)
-    .subscribe(data => {
-      this.intercambios = data;
+  cargarHistorial() {
+    if (!this.cursoId) {
+      console.error('cursoId inválido');
+      return;
+    }
+
+    this.http.get<any[]>(`http://localhost:8000/api/cursos/historial-intercambios/?curso=${this.cursoId}`)
+      .subscribe(data => {
+        this.intercambios = data;
+      });
+  }
+
+  cargarPegatinas() {
+    this.http.get<any[]>('http://localhost:8000/api/cursos/mis-pegatinas/').subscribe(data => {
+      this.pegatinas = data.sort(() => 0.5 - Math.random()).slice(0, 2);
     });
-}
-
-
+  }
 
   volverAlCurso() {
     this.router.navigate(['/curso', this.cursoId]);
   }
-  
 }
